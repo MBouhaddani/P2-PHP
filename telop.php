c35d9fe1fa6b2be8247d51fd3ab14e0ee32124e3
MBouhaddani <9021551@student.zadkine.nl>
2023-11-22 11:47:28 +0100

    v5

---
+++ telop.php
@@ -0,0 +1,19 @@
+<?php
+// FILEPATH: /c:/xampp/htdocs/P2-PHP/telop.php
+
+//* Auteur: Mohamed B
+//* Datum: 22-11-2023
+//* functie: optellen
+
+$getal = 2000;
+
+// optellen 1000 + 2000
+$uitkomst = $getal + 1000;
+
+echo "De uitkomst is $uitkomst <br>";
+
+// $uikomst verhogen met 10%
+$uitkomst * 1.1;
+
+?>
+
